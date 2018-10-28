9c2fac27831a5c8c40ef4440c7a033c7d2a5afcb
kan.notice <kan@notice.co.jp>
2018-10-28 13:41:31 +0900

    add fulfillment function for Action on Google Assistance

--- functions/index.js
+++ functions/index.js
@@ -1,3 +1,6 @@
+'use strict';
+process.env.DEBUG = 'dialogflow:debug'; // enables lib debugging statements
+
 const admin = require('firebase-admin');
 const functions = require('firebase-functions');
 const moment = require('moment');
@@ -71,6 +74,25 @@ function updateBestTime(best, record) {
   return best;
 }
 
+function convertRecordedAt(date) {
+  const d = moment(date.substr(0, 8) + ' ' + date.substr(8));
+  return d.format('YYYY年MM月DD日のHH時mm分');
+}
+
+function replyCourseRecord(courseRecord) {
+  let reply = '';
+  if (courseRecord.exists) {
+    const best = courseRecord.data();
+    let recordedAt = convertRecordedAt(best.lapTime.recordedAt);
+    reply = `もっとも速かったラップタイムは${recordedAt}の${parseFloat(best.lapTime.time).toFixed(2)}です。`;
+    recordedAt = convertRecordedAt(best.totalTime.recordedAt);
+    reply += `もっとも速かったトータルタイムは${recordedAt}の${parseFloat(best.totalTime.time).toFixed(2)}です。`;
+    return reply;
+  } else {
+    return 'いまのところ、記録はありません。';
+  }
+}
+
 exports.uploadResults = functions.https.onRequest((request, response) => {
 
   if (request.query.results) {
@@ -93,4 +115,48 @@ exports.uploadResults = functions.https.onRequest((request, response) => {
   } else {
     response.send("no results.");
   }
+});
+
+const {WebhookClient} = require('dialogflow-fulfillment');
+
+exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, response) => {
+  const agent = new WebhookClient({ request, response });
+
+  function welcome(agent) {
+    agent.add(`こんにちは、コースマネージャです。`);
+  }
+
+  function fallback(agent) {
+    agent.add(`わかりません。`);
+    agent.add(`ごめんなさい。もう一度お願いします。`);
+  }
+
+  function queryCourseRecord(agent) {
+    console.log('call queryCourseRecord');
+    return db.collection('results').doc('CourseRecord').get()
+      .then(courseRecord => {
+        agent.add(replyCourseRecord(courseRecord));
+        return Promise.resolve('complete');
+      })
+      .catch(error => {
+        agent.add(error.message);
+      });
+  }
+  // Run the proper function handler based on the matched Dialogflow intent name
+  let intentMap = new Map();
+  intentMap.set('Default Welcome Intent', welcome);
+  intentMap.set('Default Fallback Intent', fallback);
+  intentMap.set('queryCourseRecord', queryCourseRecord);
+  // intentMap.set('your intent name here', googleAssistantHandler);
+  agent.handleRequest(intentMap);
+});
+
+exports.debug = functions.https.onRequest((request, response) => {
+  db.collection('results').doc('CourseRecord').get()
+    .then(courseRecord => {
+      response.send(replyCourseRecord(courseRecord));
+    })
+    .catch(error => {
+      response.send(error.message);
+    });
 });
